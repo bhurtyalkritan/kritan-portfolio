782f24694b07c52e67e3e747e2d959787db3feb4
Kritan Bhurtyal <kbhurtya@unc.edu>
2024-12-29 11:49:12 -0800

    temp

--- pages/index.js
+++ pages/index.js
@@ -55,9 +55,10 @@ const goals = [
 const aboutImages = ['/about-images/about-image1.jpg', '/about-images/about-image2.jpg', '/about-images/about-image3.jpg'];
 
 const experiences = [
-  { title: 'Product Support Engineer Intern', company: 'Tesla', duration: 'Sep 2024 - Dec 2024' },
-  { title: 'PM Intern', company: 'Lenovo', duration: 'June 2024 - July 2024' },
   { title: 'Backend Software Engineer', company: 'App Team Carolina', duration: 'Feb 2024 - Present' },
+  { title: 'Undergrad Research Assistant', company: 'UNC Medical School', duration: 'July 2024 - Present' },
+  { title: 'Software Engineer Intern', company: 'Tesla', duration: 'Sep 2024 - Dec 2024' },
+  { title: 'Technical Product Management Intern', company: 'Lenovo', duration: 'June 2024 - July 2024' },
   { title: 'Software Engineer Intern', company: 'Amtev', duration: 'May 2023 - Aug 2023' },
 ];
 
